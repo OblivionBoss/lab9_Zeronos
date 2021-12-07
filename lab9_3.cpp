029dbaf11e6e68ba590fe5efbcecfe32c6e16c1b
Zeronos <boss16805@gmail.com>
2021-12-07 16:46:30 +0700

    lab9_3 completed

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,47 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int age,height,property;
+    string status[] = {"MARRIED","ONE-NIGHT-STAND","BEST FRIEND","FRIEND","UNFRIEND"};
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age <= 20){
+        cout << "Enter your height: ";
+        cin >> height;
+        if(height < 160){
+            cout << "Your status = " << status[4];
+        }
+        else if(height < 175){
+            cout << "Your status = " << status[3];
+        }
+        else{
+            cout << "Enter your property: ";
+            cin >> property;
+            if(property > 69000000){
+                cout << "Your status = " << status[0];
+            }
+            else{
+                cout << "Your status = " << status[1];
+            }
+        }
+    }
+    else if(age < 30){
+        cout << "Enter your property: ";
+        cin >> property;
+        if(property > 10000000){
+            cout << "Your status = " << status[2];
+        }
+        else{
+            cout << "Your status = " << status[4];
+        }
+    }
+    else{
+        cout << "Your status = " << status[4];
+    }
+return 0;
+}
 /*
 "Enter your age: "
 "Enter your height: "
